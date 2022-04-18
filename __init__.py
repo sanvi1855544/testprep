589530807b6043194b6f1ea3c8404fb6d99b10a0
sanvi1855544 <sanvi11929@gmail.com>
2022-04-17 17:23:08 -0700

    add login stuff

--- __init__.py
+++ __init__.py
@@ -1,4 +1,20 @@
 from flask import Flask
 
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
 """This needs to be isolated to support blueprints and models"""
 app = Flask(__name__)
+
+dbURI = 'sqlite:///model/myDB.db'
+# Setup SQLAlchemy object and properties for the database (db)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = dbURI
+app.config['SECRET_KEY'] = 'SECRET_KEY'
+db = SQLAlchemy(app)
+Migrate(app, db)
+# Setup LoginManager object (app)
+login_manager = LoginManager()
+login_manager.init_app(app)
--- crud/model.py
+++ crud/model.py
@@ -3,8 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 from flask_migrate import Migrate
 
-from __init__ import app
+from __init__ import app, db
 
+from sqlalchemy.exc import IntegrityError
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
 # Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along
@@ -24,7 +27,7 @@ Migrate(app, db)
 # -- a.) db.Model is like an inner layer of the onion in ORM
 # -- b.) Users represents data we want to store, something that is built on db.Model
 # -- c.) SQLAlchemy ORM is layer on top of SQLAlchemy Core, then SQLAlchemy engine, SQL
-class Users(db.Model):
+class Users(UserMixin, db.Model):
     # define the Users schema
     userID = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), unique=False, nullable=False)
@@ -36,7 +39,7 @@ class Users(db.Model):
     def __init__(self, name, email, password, phone):
         self.name = name
         self.email = email
-        self.password = password
+        self.set_password(password)
         self.phone = phone
 
     # CRUD create/add a new record to the table
@@ -59,7 +62,8 @@ class Users(db.Model):
             "name": self.name,
             "email": self.email,
             "password": self.password,
-            "phone": self.phone
+            "phone": self.phone,
+            "query": "by_san"
         }
 
     # CRUD update: updates users name, password, phone
@@ -69,7 +73,7 @@ class Users(db.Model):
         if len(name) > 0:
             self.name = name
         if len(password) > 0:
-            self.password = password
+            self.set_password(password)
         if len(phone) > 0:
             self.phone = phone
         db.session.commit()
@@ -82,42 +86,57 @@ class Users(db.Model):
         db.session.commit()
         return None
 
+    # set password method is used to create encrypted password
+    def set_password(self, password):
+        """Create hashed password."""
+        self.password = generate_password_hash(password, method='sha256')
+
+    # check password to check versus encrypted password
+    def is_password_match(self, password):
+        """Check hashed password."""
+        result = check_password_hash(self.password, password)
+        return result
+
+    # required for login_user, overrides id (login_user default) to implemented userID
+    def get_id(self):
+        return self.userID
 
 """Database Creation and Testing section"""
 
+def model_tester():
+    print("--------------------------")
+    print("Seed Data for Table: users")
+    print("--------------------------")
+    db.create_all()
+    """Tester data for table"""
+    u1 = Users(name='Sanvi Pal', email='sanvi11929@gmail.com', password='rad1234', phone="8587059884")
+    u2 = Users(name='Shruti Chari', email='shrutijchari@gmail.com', password='123shrut', phone="1111112222")
+    u3 = Users(name='Gennalyn Bongalan', email='gennb@gmail.com', password='123genn', phone="1111113333")
+    u4 = Users(name='Kian Pasokhi', email='kianp@gmail.com', password='123kian', phone="1111114444")
+    u5 = Users(name='John Mortensen', email='jmort1021@gmail.com', password='123qwerty', phone="8587754956")
+    u6 = Users(name='John Mortensen', email='jmort1021@yahoo.com', password='123qwerty', phone="8587754956")
+    # U7 intended to fail as duplicate key
+    u7 = Users(name='John Mortensen', email='jmort1021@yahoo.com', password='123qwerty', phone="8586791294")
+    table = [u1, u2, u3, u4, u5, u6, u7]
+    for row in table:
+        try:
+            db.session.add(row)
+            db.session.commit()
+        except IntegrityError:
+            db.session.remove()
+            print(f"Records exist, duplicate email, or error: {row.email}")
 
 
-# def model_tester():
-# print("--------------------------")
-# print("Seed Data for Table: users")
-# print("--------------------------")
-# db.create_all()
-# """Tester data for table"""
-# u1 = Users(name='Akhil Nandhakumar', email='akhil@nandhakumar.net', password='codecodecode', phone="1111111111")
-# u2 = Users(name='Nicholas Tesla', email='ntesla@example.com', password='123niko', phone="1111112222")
-# u3 = Users(name='Alexander Graham Bell', email='agbell@example.com', password='123lex', phone="1111113333")
-# u4 = Users(name='Eli Whitney', email='eliw@example.com', password='123whit', phone="1111114444")
-# u5 = Users(name='John Mortensen', email='jmort1021@gmail.com', password='123qwerty', phone="8587754956")
-# table = [u1, u2, u3, u4, u5]
-# for row in table:
-# try:
-# db.session.add(row)
-# db.session.commit()
-# except IntegrityError:
-# db.session.remove()
-# print(f"Records exist, duplicate email, or error: {row.email}")
+def model_printer():
+    print("------------")
+    print("Table: users with SQL query")
+    print("------------")
+    result = db.session.execute('select * from users')
+    print(result.keys())
+    for row in result:
+        print(row)
 
 
-# def model_printer():
-# print("------------")
-# print("Table: users with SQL query")
-# print("------------")
-# result = db.session.execute('select * from users')
-# print(result.keys())
-# for row in result:
-# print(row)
-
-
-# if __name__ == "__main__":
-# model_tester()  # builds model of Users
-# model_printer()
+if __name__ == "__main__":
+    model_tester()  # builds model of Users
+    model_printer()
